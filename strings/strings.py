04effd5dabc120c30d61f3c00a0ec6cba30afffb
Yurubhillow <hillowyurub@gmail.com>
2026-06-09 15:49:16 +0300

    added string methods

--- strings/strings.py
+++ strings/strings.py
@@ -5,3 +5,38 @@ print(len(my_name))
 
 course = "IT"
 print("I'm persuing ", course)
+
+# String methods- single line comment
+""""
+multple line comments
+"""
+print("any text")
+country = "kenya"
+print("My country is : ", country)
+
+country_caps = country.upper()  # upper is the string method converts to upper case
+print(country_caps)
+
+# lower is the string method converts to lower case
+country_lower = country_caps.lower()
+print(country_lower)
+
+# startswith
+print(country_caps.startswith("K"))
+
+# endswith
+print(country_caps.endswith("Z"))
+
+# replace method
+print(country_caps.replace("KENYA", "TANZANIA"))
+
+# strip method- used to clear whitespaces before and after a variable
+dog = " Chiwawa "
+print(dog)
+print(dog.strip())
+
+# rstrip - clears from the right side of the variable
+print(dog.rstrip())
+
+# lstrip - clear from the left side of the variable
+print(dog.lstrip())
