d876f92080d2680e3383b203b6243270c8547e8c
Yurubhillow <hillowyurub@gmail.com>
2026-06-10 14:36:52 +0300

    added list items

---
+++ list/lists.py
@@ -0,0 +1,11 @@
+''''
+list are ordered, changeable, allow duplicates, are indexed and created using square brackets
+'''
+fruits = ["banana", "mango", "orange", "dragonfruit", "watermelon"]
+print(f"list of fruits: {fruits}")
+print(f"data type: {type(fruits)}")
+print(f"the 4th fruit is : {fruits[3]}")
+
+# slice
+print(fruits[2:])
+print(fruits[1:3])
